67f11150f4bcf285325e3cafd8630017c01e545f
Brandon Herford <brandonherford@Brandons-MBP.fios-router.home>
2024-01-24 15:33:59 -0500

    Add spline

---
+++ app.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/GpBYvJvkwLSGgw3B/scene.splinecode');
